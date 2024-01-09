1c3527efe052e7350d23e34c36a70867b7e43c38
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-09 23:04:36 +0300

    update build page

--- src/pages/AddNewTodo.jsx
+++ src/pages/AddNewTodo.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddNewTodo() {
+
+    const [formData, setFormData] = useState({
+        task: '',
+        description: ''
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        })
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        try {
+            const res = await fetch('http://localhost:4001/api/addTask', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            if (!res.ok) {
+                console.log('An error occured')
+            } else {
+                const data = await res.json()
+                console.log(data)
+                setFormData({task: '', description: ''})
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return (
-        <h1>This is AddNewTodo page</h1>
+        <div className="addNewTodo-page">
+            <form onSubmit={handleSubmit} className="addTodo-form">
+                <div className="form-item">
+                    <label htmlFor="task">Task</label>
+                    <input
+                        type="text"
+                        name="task"
+                        id="task"
+                        value={formData.task}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="form-item">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        type="text"
+                        rows={5}
+                        name="description"
+                        id="description"
+                        value={formData.description}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
     )
 }
