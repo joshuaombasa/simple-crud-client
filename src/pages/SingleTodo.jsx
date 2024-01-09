1487396e87272148570923f60890c29a90fd7352
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-09 23:03:55 +0300

    update build page

--- src/pages/SingleTodo.jsx
+++ src/pages/SingleTodo.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function SingleTodo() {
+
+    const [todo, setTodo] = useState(null)
+
+    const params = useParams()
+
+    useEffect(() => {
+        async function getTodo() {
+            try {
+                const res = await fetch(`http://localhost:4001/api/todos/${params.id}`)
+                if (!res.ok) {
+                    console.log('An error occured')
+                } else {
+                    const data = await res.json()
+                    console.log(data)
+                    setTodo(data)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getTodo()
+    },[])
+
+    if (!todo) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+
+
+
     return (
-        <h1>This is SingleTodo page</h1>
+        <div className="singleTodo-page">
+            <Link 
+                to=".."
+                relative="path"
+            > &larr; Back to all todos</Link>
+            <h3>{todo.task}</h3>
+            <p>{todo.description}</p>
+        </div>
     )
 }
